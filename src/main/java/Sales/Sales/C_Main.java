9c785b120bade4b59f25b9645674dc0abcee6d6d
MadiOrazayev <x.madi@mail.ru>
2023-05-18 17:37:38 +0300

    14may

--- src/main/java/Sales/Sales/C_Main.java
+++ src/main/java/Sales/Sales/C_Main.java
@@ -1,12 +1,13 @@
 package Sales.Sales;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class C_Main {
     @GetMapping(value = "/")
-    public String index(){
-        return "Main_layout";
+    public String index(Model model){
+        return "main_layout";
     }
 }
